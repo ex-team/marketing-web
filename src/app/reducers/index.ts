97cddc106b8483c4bed3953494a4c91203daf473
tri-evendi <evendyx@gmail.com>
2021-07-25 02:43:46 +0700

    Updates

--- src/app/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-
-import prefsReducer from './prefs';
-
-const rootReducer = combineReducers({prefs: prefsReducer });
-export { rootReducer, prefsReducer };
